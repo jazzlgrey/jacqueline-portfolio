5e71595f2b2cfbe9aea9c1d91e2e7645ad3c5943
Jacqueline Song <itsjacquelinehere@gmail.com>
2026-07-06 12:51:45 -0700

    Add projects page and raster sketch

---
+++ sketches/raster-sketch.js
@@ -0,0 +1,32 @@
+let img;
+
+function preload() {
+  img = loadImage("../assets/images/nara.jpg");
+}
+
+function setup() {
+  let canvas = createCanvas(800, 600);
+  canvas.parent("raster-container");
+
+  img.resize(width, height);
+  noStroke();
+}
+
+function draw() {
+  background(248, 241, 236);
+
+  let tileSize = map(mouseX, 0, width, 6, 28);
+  tileSize = constrain(tileSize, 6, 28);
+
+  for (let y = 0; y < height; y += tileSize) {
+    for (let x = 0; x < width; x += tileSize) {
+      let c = img.get(x, y);
+      let b = brightness(c);
+
+      let circleSize = map(b, 0, 255, tileSize * 1.2, 1);
+
+      fill(c);
+      ellipse(x, y, circleSize, circleSize);
+    }
+  }
+}
